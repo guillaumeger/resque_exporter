9f3ceb95cb479a73a171ea9ace200df846fa6122
Guillaume Germain <g.germain@samsung.com>
2020-06-19 15:12:44 -0400

    Working prototype

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/guillaumeger/resque_exporter
+
+go 1.14
+
+require (
+	github.com/go-redis/redis v6.15.8+incompatible
+	github.com/onsi/ginkgo v1.13.0 // indirect
+	github.com/prometheus/client_golang v1.7.0
+)
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"net/http"
 
@@ -12,18 +11,20 @@ import (
 )
 
 func main() {
-	var ctx = context.Background()
+	// var ctx = context.Background()
 	red := redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379",
 		Password: "",
 		DB:       0,
 	})
 
-	// info := red.Info(ctx)
-	// fmt.Println(info)
-	keys := red.Keys(ctx, "resque:*")
+	info := red.Info()
+	fmt.Println(info)
+
+	keys := red.Keys("resque:*")
 	fmt.Println(keys)
-	workers, err := red.SMembers(ctx, "resque:workers").Result()
+
+	workers, err := red.SMembers("resque:workers").Result()
 	if err != nil {
 		fmt.Printf("An error occured: %s\n", err)
 	}
@@ -35,6 +36,89 @@ func main() {
 		Help: "Number of workers",
 	})
 	wNo.Set(workersNo)
+
+	wn := 0.0
+	for _, w := range workers {
+		exist, err := red.Exists("resque:worker" + w).Result()
+		if err != nil {
+			fmt.Printf("An error occured: %s\n", err)
+		}
+		if exist == 1 {
+			wn++
+		}
+	}
+	fmt.Println(wn)
+	prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "resque_workers_working",
+		Help: "Number of workers currently working",
+	}).Set(wn)
+
+	q, err := red.SMembers("resque:queues").Result()
+	if err != nil {
+		fmt.Printf("An error occured: %s\n", err)
+	}
+
+	fmt.Println(q)
+
+	if len(q) != 0 {
+		for _, qName := range q {
+			qJobs, err := red.LLen("queue" + qName).Result()
+			if err != nil {
+				fmt.Printf("An error occured: %s\n", err)
+			}
+			qJobsNo := promauto.NewGauge(prometheus.GaugeOpts{
+				Name: "resque_queue_jobs",
+				Help: "Number of jobs in queue",
+				ConstLabels: prometheus.Labels{
+					"queue": qName,
+				},
+			})
+			qJobsNo.Set(float64(qJobs))
+		}
+	}
+
+	promauto.NewCounterFunc(prometheus.CounterOpts{
+		Name: "resque_jobs_proccesed_total",
+		Help: "Total number of proccesed jobs",
+	}, func() float64 {
+		p, err := red.Get("resque:stat:processed").Float64()
+		if err != nil {
+			fmt.Printf("An error occured: %s\n", err)
+		}
+		return p
+	})
+
+	promauto.NewCounterFunc(prometheus.CounterOpts{
+		Name: "resque_jobs_failed_total",
+		Help: "Total number of failed jobs",
+	}, func() float64 {
+		f, err := red.Get("resque:stat:failed").Float64()
+		if err != nil {
+			fmt.Printf("An error occured: %s\n", err)
+		}
+		return f
+	})
+
+	fqNo := promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "resque_failed_queue",
+		Help: "Number of jobs in the failed queue",
+	})
+
+	fq, err := red.LLen("resque:failed").Result()
+	if err != nil {
+		fmt.Printf("An error occured: %s\n", err)
+	}
+
+	fqNo.Set(float64(fq))
+
 	http.Handle("/metrics", promhttp.Handler())
 	http.ListenAndServe(":9112", nil)
+}
+
+func getJobs(c *redis.Client, k string) float64 {
+	j, err := c.Get(k).Float64()
+	if err != nil {
+		fmt.Printf("An error occured: %s\n", err)
+	}
+	return j
 }
